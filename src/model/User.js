b22a96bff1c356aa969f230786950d057a6ca19a
KimMinJae <rlaaksen112@naver.com>
2023-04-20 23:52:57 +0900

    23.04.20

--- src/model/User.js
+++ src/model/User.js
@@ -7,11 +7,6 @@ class User {
     }
 
     login() {
-        // const body = this.body; const user = UserStorage.getUserInfo(body.id); if
-        // (user) {     if (user.id == body.id && user.pw == body.pw) {         return
-        // {success: true};     }     return {success: false, msg: "비밀번호가 틀렸습니다."};
-        // }return {success: false, msg: "없는 아이디 입니다."};
-
         const body = this.body;
         const user = UserStorage.getUserInfo(body.id);
         console.log(user);      /* undefined == 값을 할당하지 않은 변수 */
--- src/public/js/login.js
+++ src/public/js/login.js
@@ -5,9 +5,10 @@ window.onload = function () {
     const id = document.querySelector('#id');
     const pw = document.querySelector('#pw');
     const btn = document.querySelector('#btn');
+    const reg = document.querySelector('#reg');
 
     btn.addEventListener('click', login);
-
+    reg.addEventListener('click', reg_block);
     function login() {
         const req1 = {
             id: id.value,
@@ -15,7 +16,6 @@ window.onload = function () {
         }
         console.log(JSON.stringify(req1));
 
-        
         fetch("/login", {
             method: "POST",
             headers: {
@@ -36,4 +36,15 @@ window.onload = function () {
             });
     }
 
+    function reg_block() {
+        let a = document.querySelector('.register-form');
+        let b = document.querySelector('.login-form');
+        if (a.style.display == 'none' || a.style.display == '') {
+            a.style.display = 'block';
+            b.style.display = 'none';
+        } else {
+            a.style.display = 'none';
+            b.style.display = 'block';
+        }
+    }
 }
