ecf8ef990a3e498f7343334daa3ac345167089fa
KimMinJae <rlaaksen112@naver.com>
2023-04-29 23:30:24 +0900

    23.04.29

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,7 +6,7 @@ const ctrl = require('./ctrl');
 
 router.get('/',ctrl.output.home);
 
-router.get('/login',ctrl.output.login); 
+router.get('/login',ctrl.output.login);     /* login get */
 router.post('/login',ctrl.process.login);   /* login post */
 
 module.exports = router;
